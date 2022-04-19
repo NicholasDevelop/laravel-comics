6897ef1a18c5aba39cc3ec5ed7768f6e223ef246
NicholasDevelop <marcaccini.n@gmail.com>
2022-04-19 15:39:03 +0200

    inseriti componenti header e footer con relativi css

---
+++ laravel/resources/views/partials/footer.blade.php
@@ -0,0 +1,81 @@
+<div>
+    <div class="bottom-link-container">
+        <div class="bottom-link">
+            <div class="comics-shop">
+                <h3 class="title-list">Dc Comics</h3>
+                <ul class="list-body">
+                    <li class="links"><a href="#">Characters</a></li>
+                    <li class="links"><a href="#">Comics</a></li>
+                    <li class="links"><a href="#">Movies</a></li>
+                    <li class="links"><a href="#">TV</a></li>
+                    <li class="links"><a href="#">Games</a></li>
+                    <li class="links"><a href="#">Videos</a></li>
+                    <li class="links"><a href="#">News</a></li>
+                </ul>
+                <h3 class="title-list">Shop</h3>
+                <ul class="list-body">
+                    <li class="links"><a href="#">Shop DC</a></li>
+                    <li class="links"><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+
+            <div class="dc">
+                <h3 class="title-list">Dc</h3>
+                <ul class="list-body">
+                    <li class="links"><a href="#">Terms Of Use</a></li>
+                    <li class="links"><a href="#">Privacy policy (New)</a></li>
+                    <li class="links"><a href="#">Ad Choices</a></li>
+                    <li class="links"><a href="#">Advertising</a></li>
+                    <li class="links"><a href="#">Jobs</a></li>
+                    <li class="links"><a href="#">Subscriptions</a></li>
+                    <li class="links"><a href="#">Talent Workshops</a></li>
+                    <li class="links"><a href="#">CPSC Certificates</a></li>
+                    <li class="links"><a href="#">Ratings</a></li>
+                    <li class="links"><a href="#">Shop Help</a></li>
+                    <li class="links"><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="sites">
+                <h3 class="title-list">Sites</h3>
+                <ul class="list-body">
+                    <li class="links"><a href="#">DC</a></li>
+                    <li class="links"><a href="#">MAD Magazine</a></li>
+                    <li class="links"><a href="#">DC Kids</a></li>
+                    <li class="links"><a href="#">DC Universe</a></li>
+                    <li class="links"><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="social-container">
+        <div class="social">
+            <button class="sign-up">
+                Sign-Up Now!
+            </button>
+
+            <div class="follow-us">
+                <h3 class="social-title">Follow Us</h3>
+                <ul class="social-list-link">
+                    <li class="social-link">
+                        <img class="special-size" src="../assets/img/footer-facebook.png" alt="">
+                    </li>
+                    <li class="social-link">
+                        <img class="special-size" src="../assets/img/footer-twitter.png" alt="">
+                    </li>
+                    <li class="social-link">
+                        <img class="special-size" src="../assets/img/footer-youtube.png" alt="">
+                    </li>
+                    <li class="social-link">
+                        <img class="special-size" src="../assets/img/footer-pinterest.png" alt="">
+                    </li>
+                    <li class="social-link">
+                        <img class="special-size" src="../assets/img/footer-periscope.png" alt="">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -1,8 +1,21 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/contact">Contact</a></li>
+<header class="header">
+    <div class="container">
+        <figure>
+            <img src="{{asset('/laravel/public/img/dc-logo.png')}}" alt="">
+        </figure>
+
+        <ul class="navbar">
+            <li class="nav-list"><a href="#">Characters</a></li>
+            <li class="nav-list active"><a class="active" href="#">Comics</a></li>
+            <li class="nav-list"><a href="#">Movies</a></li>
+            <li class="nav-list"><a href="#">Tv</a></li>
+            <li class="nav-list"><a href="#">Games</a></li>
+            <li class="nav-list"><a href="#">Collectibles</a></li>
+            <li class="nav-list"><a href="#">Videos</a></li>
+            <li class="nav-list"><a href="#">Fans</a></li>
+            <li class="nav-list"><a href="#">News</a></li>
+            <li class="nav-list"><a href="#">Shop</a></li>
         </ul>
-    </nav>
+
+    </div>
 </header>
